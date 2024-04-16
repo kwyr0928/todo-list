a6b1bac8273a67ed762cd81a57dec6968f7cec49
kwyr0928 <yuri.kawase.0928@gmail.com>
2024-04-17 01:25:05 +0900

    簡単な機能の追加

--- todo.js
+++ todo.js
@@ -1,10 +1,11 @@
 function addTask () {
     // 入力された値をtaskに代入
     const task = document.getElementById('input');
+    const date = document.getElementById('date');
     // ulを取得 ここにliを足していく
     const list = document.getElementById('ul');
     const listItem = document.createElement('li');
-    listItem.textContent = task.value;
+    listItem.textContent = task.value + "   " + date.value;
     list.appendChild(listItem);
     task.value = "";
 }
